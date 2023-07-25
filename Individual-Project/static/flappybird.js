3cb982c229772267cfc5bf75dfb567fd19b5879f
razifa24-meet <139681113+razifa24-meet@users.noreply.github.com>
2023-07-25 15:47:47 +0300

    finally the game works

--- Individual-Project/static/flappybird.js
+++ Individual-Project/static/flappybird.js
@@ -49,16 +49,17 @@ window.onload = function() {
 
     //load images
     birdImg = new Image();
-    birdImg.src = "./flappybird.png";
+    birdImg.src = "static/./flappybird.png";
     birdImg.onload = function() {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
 
     topPipeImg = new Image();
-    topPipeImg.src = "./toppipe.png";
+    topPipeImg.src = "static/./toppipe.png";
+
 
     bottomPipeImg = new Image();
-    bottomPipeImg.src = "./bottompipe.png";
+    bottomPipeImg.src = "static/./bottompipe.png";
 
     requestAnimationFrame(update);
     setInterval(placePipes, 1500); //every 1.5 seconds
